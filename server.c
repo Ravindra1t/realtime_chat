df25b4f4d567b894b89eb3d97be4dd5b1faf0a6d
Ravindra1t <143156576+Ravindra1t@users.noreply.github.com>
2024-12-03 23:06:30 +0530

    Update server.c

--- server.c
+++ server.c
@@ -8,10 +8,40 @@
 
 #define PORT 8080
 #define BUF_SIZE 1024
-#define PASSWORD_FILE "server_password.txt"
+#define PASSWORD_FILE "client_password.txt"
 
 const unsigned char AES_KEY[] = "0123456789012345";
 const unsigned char AES_IV[] = "0123456789012345";
+
+int aes_encrypt(const unsigned char *plaintext, int plaintext_len, unsigned char *key,
+                unsigned char *iv, unsigned char *ciphertext) {
+    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+    int len, ciphertext_len;
+
+    EVP_EncryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, key, iv);
+    EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len);
+    ciphertext_len = len;
+    EVP_EncryptFinal_ex(ctx, ciphertext + len, &len);
+    ciphertext_len += len;
+
+    EVP_CIPHER_CTX_free(ctx);
+    return ciphertext_len;
+}
+
+int aes_decrypt(const unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
+                unsigned char *iv, unsigned char *plaintext) {
+    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+    int len, plaintext_len;
+
+    EVP_DecryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, key, iv);
+    EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len);
+    plaintext_len = len;
+    EVP_DecryptFinal_ex(ctx, plaintext + len, &len);
+    plaintext_len += len;
+
+    EVP_CIPHER_CTX_free(ctx);
+    return plaintext_len;
+}
 
 void hash_password(const char *password, unsigned char *hash) {
     SHA256((unsigned char *)password, strlen(password), hash);
@@ -19,9 +49,9 @@ void hash_password(const char *password, unsigned char *hash) {
 
 void prompt_for_password(char *password, int is_new) {
     if (is_new)
-        printf("Set a new server password: ");
+        printf("Set a new client password: ");
     else
-        printf("Enter server password: ");
+        printf("Enter client password: ");
 
     fgets(password, BUF_SIZE, stdin);
     password[strcspn(password, "\n")] = 0; // Remove newline
@@ -66,99 +96,53 @@ int verify_password() {
     }
 }
 
-int aes_encrypt(const unsigned char *plaintext, int plaintext_len, unsigned char *key,
-                unsigned char *iv, unsigned char *ciphertext) {
-    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
-    int len, ciphertext_len;
-
-    EVP_EncryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, key, iv);
-    EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len);
-    ciphertext_len = len;
-    EVP_EncryptFinal_ex(ctx, ciphertext + len, &len);
-    ciphertext_len += len;
-
-    EVP_CIPHER_CTX_free(ctx);
-    return ciphertext_len;
-}
-
-int aes_decrypt(const unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
-                unsigned char *iv, unsigned char *plaintext) {
-    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
-    int len, plaintext_len;
-
-    EVP_DecryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, key, iv);
-    EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len);
-    plaintext_len = len;
-    EVP_DecryptFinal_ex(ctx, plaintext + len, &len);
-    plaintext_len += len;
-
-    EVP_CIPHER_CTX_free(ctx);
-    return plaintext_len;
-}
-
 int main() {
     if (!verify_password()) {
         exit(EXIT_FAILURE);
     }
 
-    printf("Server starting...\n");
+    printf("Client starting...\n");
 
-    int server_fd, new_socket;
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
+    int sock = 0;
+    struct sockaddr_in serv_addr;
     unsigned char buffer[BUF_SIZE] = {0};
     unsigned char encrypted[BUF_SIZE] = {0};
     unsigned char decrypted[BUF_SIZE] = {0};
 
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("Socket failed");
-        exit(EXIT_FAILURE);
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Socket creation error");
+        return -1;
     }
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
 
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("Bind failed");
-        exit(EXIT_FAILURE);
+    if (inet_pton(AF_INET, "192.168.117.161", &serv_addr.sin_addr) <= 0) {
+        perror("Invalid address / Address not supported");
+        return -1;
     }
 
-    if (listen(server_fd, 3) < 0) {
-        perror("Listen failed");
-        exit(EXIT_FAILURE);
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Connection failed");
+        return -1;
     }
 
-    printf("Server listening on port %d\n", PORT);
-
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
-        perror("Accept failed");
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Client connected\n");
+    printf("Connected to server\n");
 
     while (1) {
-        int bytes_read = read(new_socket, encrypted, BUF_SIZE);
-        if (bytes_read <= 0) {
-            break;
-        }
-
-        int decrypted_len = aes_decrypt(encrypted, bytes_read, (unsigned char *)AES_KEY, (unsigned char *)AES_IV, decrypted);
-        decrypted[decrypted_len] = '\0';
-        printf("Client: %s\n", decrypted);
-
         printf("Enter message: ");
         fgets((char *)buffer, BUF_SIZE, stdin);
         buffer[strcspn((char *)buffer, "\n")] = '\0';
 
         int encrypted_len = aes_encrypt(buffer, strlen((char *)buffer), (unsigned char *)AES_KEY, (unsigned char *)AES_IV, encrypted);
-        send(new_socket, encrypted, encrypted_len, 0);
+        send(sock, encrypted, encrypted_len, 0);
+
+        int bytes_read = read(sock, encrypted, BUF_SIZE);
+        int decrypted_len = aes_decrypt(encrypted, bytes_read, (unsigned char *)AES_KEY, (unsigned char *)AES_IV, decrypted);
+        decrypted[decrypted_len] = '\0';
+        printf("Server: %s\n", decrypted);
     }
 
-    close(new_socket);
-    close(server_fd);
-
+    close(sock);
     return 0;
 }
-
